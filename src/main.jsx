4db9f658593a6012eac2b0b1316fe33aec0f67fd
Pamela Suarez <paracuentaspomme@gmail.com>
2026-06-25 14:54:47 -0600

    Change into hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <CartProvider>
       <App />
     </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
